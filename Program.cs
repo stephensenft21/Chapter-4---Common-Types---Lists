4f1c1410280fe6f770afe0ec1134a64f06d54599
stephensenft21 <stephensenft21@gmail.com>
2019-11-15 11:18:20 -0600

    set up a list in c#

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Security.Cryptography;
+using System;
+using System.Collections.Generic;
+
+namespace listinfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        List<int> cohorts = new List<int>(); 
+        cohorts.Add(34);
+        cohorts.Add(35);
+        cohorts.Add(36);
+        cohorts.Add(37);
+        //Another way we can init this list
+        List<int> classes = new List<int>()
+        {
+         34,
+         35,
+         36,
+         37,
+
+        };
+
+        classes.Remove(34);
+        classes.Add(38);
+        
+        foreach(int nssClass in cohorts){
+            
+            if(nssClass == 35)
+            {
+            Console.WriteLine($"The Best Cohort: {nssClass}");
+            }
+            else
+            {
+                Console.WriteLine($"A Perfectly fine Cohort: {nssClass}");
+            }
+
+        }
+
+
+        }
+    
+    }
+}
